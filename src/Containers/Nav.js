79853557a8900a3426e0101f2f2c059723eb3fa5
Locallize <Fillanlee@aol.com>
2020-03-03 20:25:52 -0500

    edit mobile landscape logo double row issues

--- src/Containers/Nav.js
+++ src/Containers/Nav.js
@@ -1,40 +1,38 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../Scss/nav.scss'
 
 export default function Nav() {
 
-    let hideBar = ()=>{
+    let hideBar = () => {
         let navBarItem = document.querySelector('.main-nav ul')
         console.log("clicked")
 
-        if ( navBarItem.classList.contains('hidden')){
+        if (navBarItem.classList.contains('hidden')) {
             navBarItem.classList.replace('hidden', 'show')
         } else {
             navBarItem.classList.replace('show', 'hidden') ||
-            navBarItem.classList.add('hidden')
+                navBarItem.classList.add('hidden')
         }
-       
+
     }
 
-    
+
     return (
         <nav className="main-nav">
-            <div className='mobile-burger' onClick={()=> hideBar()}>
+            <div className='mobile-burger' onClick={() => hideBar()}>
                 <FontAwesomeIcon icon={['fa', 'bars']} size="2x" />
                 <div className='logo'>
-                {'//'} Fillan
+                    {'//'} Fillan
                 </div>
             </div>
-                <div className="welcome">
-                    <h2>
-                {'//'} Fillan
-                    </h2>
-                </div>
-            <ul 
-            // className="hidden"
-            >
+            <div className="welcome">
+                <h2>
+                    {'//'} Fillan
+                </h2>
+            </div>
+            <ul>
                 <li>
                     <Link to='/'>
                         Home
@@ -42,7 +40,7 @@ export default function Nav() {
                 </li>
                 <li>
                     <Link to='/projects'>
-                    Projects
+                        Projects
                     </Link>
                 </li>
                 {/* <li>
@@ -55,7 +53,7 @@ export default function Nav() {
                         Contact
                     </Link>
                 </li>
-            </ul> 
+            </ul>
         </nav>
     )
 }
